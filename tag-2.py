f8013e08063a5b6375f2cf634ff9c2149f3d40ae
emilie <emilie.die@icloud.com>
2023-12-28 22:01:11 +0100

    learning function

---
+++ tag-2.py
@@ -0,0 +1,139 @@
+# def add(x, y):
+#     z = x + y
+#     return z
+#
+#
+# print(add(155, 100))
+#
+
+def add(a, b, c):
+    z = a + b + c
+    return z
+
+
+add(15, 60, 90)
+
+
+def muliti(a, b):
+    c = a * b
+    return c
+
+
+x = muliti(8, 9)
+print(x)
+
+
+def enpacken(cloths, glass):
+    print(f"package is done, I wear {cloths} with {glass}")
+
+
+def driving(car, package, start, end):
+    print(f"My {car} with {package} is ready to go from {start} to {end}")
+
+
+def swimming(package, cost):
+    print(f"we are swimming now with {package}, paid {cost} euro ")
+
+
+enpacken("swimming cloth", "swimming glass")
+driving("Audi A1", "Pink Package", "Home", "Vorksbad")
+swimming("meine paket", 5)
+
+
+def subt(a, b):
+    c = a - b
+    return c
+
+
+print(subt(50, 20))
+
+
+def mulpliti(a,b):
+    c=a*b
+    return c
+
+print(mulpliti(5,5))
+
+
+def add(a,b):
+    c = a +b
+    return c
+
+print(add (10055,10033))
+
+def add(a,b):
+    c=a+b
+    return c
+
+print(add (125,125))
+
+
+def  add(a,b):
+    c =a+b
+    return c
+
+print(add (352,452))
+
+
+def add(a,b):
+    c=a+b
+    return c
+
+print(add(46375,564))
+
+
+def add(a,b):
+
+
+    c=a+b
+    return c
+
+
+
+print(add(5374,79580))
+
+def add (a,b):
+    c=a+b
+    return c
+
+print(add (4636,564))
+
+
+def add (a,b):
+    c=a+b
+    return c
+
+print(add(563,7859))
+
+
+def geteilt(a,b):
+    c=a*b
+    return c
+
+print(geteilt(3643,6))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
